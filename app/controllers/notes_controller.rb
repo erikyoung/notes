b8f4956b13f9a45fbedb1a0c835c4b9308486b04
erikyoung <erik.young@rmit.edu.vn>
2017-08-28 22:12:48 +0700

    added show validation

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,6 +5,11 @@ class NotesController < ApplicationController
 		render json: notes
 	end
 
+	def show
+		note = Note.find(params[:id])
+		render json: note
+	end
+
 	def create
 		note = Note.create(note_params)
 		if note.valid?
